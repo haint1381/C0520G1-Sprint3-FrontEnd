315248e220e621ccc0c6ce7fd361e22c0c782afd
Nguyentienhai1308 <tienhai19631959@gmail.com>
2020-12-22 16:58:25 +0700

    hello

--- sprint-3/internet-store-management/src/app/app.module.ts
+++ sprint-3/internet-store-management/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {ComputerManagerModule} from './computer-manager/computer-manager.module';
+import {PageCommonModule} from './page-common/page-common.module';
+import {PayManagerModule} from './pay-manager/pay-manager.module';
+import {ServiceRequestManagerModule} from './service-request-manager/service-request-manager.module';
+import {ServicesManagerModule} from './services-manager/services-manager.module';
+import {UserManagerModule} from './user-manager/user-manager.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComputerManagerModule,
+    PageCommonModule,
+    PayManagerModule,
+    ServiceRequestManagerModule,
+    ServicesManagerModule,
+    UserManagerModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
