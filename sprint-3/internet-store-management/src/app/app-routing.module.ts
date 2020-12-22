015f9127cf4cadf03049b4014afa2e0523ce8f97
Nguyentienhai1308 <tienhai19631959@gmail.com>
2020-12-22 16:55:31 +0700

    hello

--- sprint-3/internet-store-management/src/app/app-routing.module.ts
+++ sprint-3/internet-store-management/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {PageDefaultComponent} from './page-common/page-default/page-default.component';
+import {LoginComponent} from './page-common/login/login.component';
 
 const routes: Routes = [
   {
