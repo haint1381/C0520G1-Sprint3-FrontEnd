16aaf4c33d0513bf29fcc7e22b4722bb965421ed
Nguyentienhai130801 <65660754+Nguyentienhai130801@users.noreply.github.com>
2020-12-29 20:42:55 +0700

    Update page-default.component.ts

--- sprint-3/internet-store-management/src/app/page-common/page-default/page-default.component.ts
+++ sprint-3/internet-store-management/src/app/page-common/page-default/page-default.component.ts
@@ -1,19 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {RequestServiceComponent} from '../../service-request-manager/component/request-service/request-service.component';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-default',
   templateUrl: './page-default.component.html',
   styleUrls: ['./page-default.component.css']
 })
 export class PageDefaultComponent implements OnInit {
-
-  constructor(
-    private dialog: MatDialog
-  ) {
-  }
-
+  constructor() { }
   ngOnInit(): void {
   }
 }
