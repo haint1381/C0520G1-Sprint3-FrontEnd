e996ccaec46428367b648352af8d21dd30d954f9
tranhuuhien112747 <tranhuuhien112747@gmail.com>
2020-12-30 18:28:39 +0700

    commit ngay 30/12

--- sprint-3/internet-store-management/src/app/service-request-manager/component/request-service/request-service.component.ts
+++ sprint-3/internet-store-management/src/app/service-request-manager/component/request-service/request-service.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {RequestServiceService} from '../../service/request-service.service';
 import {Service} from '../../model/service.class';
-import {TypeServices} from '../../model/typeServices.class';
+import {UserService} from '../../../user-manager/service/user.service';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-request-service',
@@ -12,38 +13,52 @@ export class RequestServiceComponent implements OnInit {
   public idUser: number;
   public listService: Service[] = [];
   public totalMoney = 0;
-  public moneyUser = 50000;
+  public moneyUser: number;
+  public formBill: FormGroup;
 
   constructor(
     private request: RequestServiceService,
+    private userService: UserService,
+    private fb: FormBuilder
   ) {
   }
 
   ngOnInit(): void {
+    this.idUser = 1;
+    this.formBill = this.fb.group({
+    });
     this.request.getListService().subscribe(data => {
       this.listService = data;
     });
+    this.userService.getByID(this.idUser).subscribe(data => {
+      this.moneyUser = data.money;
+    });
   }
 
-  totalMoneyAll(idService: number, quanlity: number): void {
+  totalMoneyAll(idService: number, quantity: number): void {
     // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < this.listService.length; i++) {
-      if (idService === this.listService[i].idService){
-        this.listService[i].quantityPurchased = quanlity;
+      if (idService === this.listService[i].idService) {
+        this.listService[i].quantityPurchased = quantity;
       }
     }
     this.totalMoney = 0;
     // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < this.listService.length; i++) {
-      if (this.listService[i].quantityPurchased === undefined){
+      if (this.listService[i].quantityPurchased === undefined) {
         this.listService[i].quantityPurchased = 0;
       }
       this.totalMoney += (this.listService[i].quantityPurchased * this.listService[i].price);
     }
   }
+
   number(value: string): number {
     return Number(value);
   }
 
 
+  // tslint:disable-next-line:typedef
+  sendData() {
+    console.log(this.formBill.value);
+  }
 }
--- sprint-3/internet-store-management/src/app/service-request-manager/service-request-manager.module.ts
+++ sprint-3/internet-store-management/src/app/service-request-manager/service-request-manager.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestServiceComponent } from './component/request-service/request-service.component';
 import {MatDialogModule} from '@angular/material/dialog';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [RequestServiceComponent],
-  imports: [
-    CommonModule,
-    MatDialogModule,
-    FormsModule
-  ]
+    imports: [
+        CommonModule,
+        MatDialogModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class ServiceRequestManagerModule { }
