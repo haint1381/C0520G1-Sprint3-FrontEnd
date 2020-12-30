b6963f00220ecba151644d7d8ed7574f39b82d44
nhatle352840 <leenhat11a1nh@gmail.com>
2020-12-30 13:57:44 +0700

    nhat commit

--- sprint-3/internet-store-management/src/app/computer-manager/component/computer-list/computer-list.component.ts
+++ sprint-3/internet-store-management/src/app/computer-manager/component/computer-list/computer-list.component.ts
@@ -10,24 +10,45 @@ import {FormGroup} from '@angular/forms';
 export class ComputerListComponent implements OnInit {
   public computers: Computer[];
   settings = {
+    add: {
+      addButtonContent: '<i class="fas fa-plus"></i>',
+      createButtonContent: '<i class="fas fa-check"></i>',
+      cancelButtonContent: '<i class="fas fa-times"></i>',
+      confirmCreate: true
+    },
+    edit: {
+      editButtonContent: '<i class="fas fa-tools"></i>',
+      saveButtonContent: '<i class="fas fa-check"></i>',
+      cancelButtonContent: '<i class="fas fa-times"></i>',
+      confirmSave: true
+    },
+    delete: {
+      deleteButtonContent: '<i class="fas fa-trash"></i>',
+    },
+    actions: {
+      title: 'Hành động',
+      position: 'right'
+    },
     columns: {
-      id: {
-        title: 'ID'
+      computerName: {
+        title: 'Tên máy'
       },
-      name: {
-        title: 'Full Name'
+      statusComputerName: {
+        title: 'Tình trạng'
       },
-      username: {
-        title: 'User Name'
+      timeStart: {
+        title: 'Thời gian bắt đầu(giờ:phút)'
       },
-      email: {
-        title: 'Email'
+      timeUser: {
+        title: 'Thời gian sử dụng'
       }
     }
   };
+
   constructor(
     public computerService: ComputerService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.computerService.getAllComputer().subscribe(data => {
@@ -36,4 +57,28 @@ export class ComputerListComponent implements OnInit {
     });
   }
 
+  onAddConfirm(event): void {
+    // if (window.confirm('Are you sure you want to save?')) {
+    // call to remote api, remember that you have to await this
+    event.confirm.resolve(event.newData);
+    console.log(event.newData);
+    this.computerService.addNewComputer(event.newData).subscribe(data => {
+      console.log(data);
+      // tslint:disable-next-line:no-unused-expression
+    }), err => {
+      console.log(err);
+    };
+  }
+  onEditConfirm(event): void {
+    // if (window.confirm('Are you sure you want to save?')) {
+    // call to remote api, remember that you have to await this
+    event.confirm.resolve(event.newData);
+    console.log(event.newData.idComputer);
+    this.computerService.editComputer(event.newData.idComputer, event.newData).subscribe(data => {
+      console.log(data);
+      // tslint:disable-next-line:no-unused-expression
+    }), err => {
+      console.log(err);
+    };
+  }
 }
