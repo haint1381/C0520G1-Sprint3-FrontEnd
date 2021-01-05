07fb85ac4b65ca93f3e020d7a2e6b687bb63427c
vosontra-01 <sontra140395@gmail.com>
2021-01-05 09:54:43 +0700

    tra commit test

--- sprint-3/internet-store-management/src/app/app-routing.module.ts
+++ sprint-3/internet-store-management/src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {PageDefaultComponent} from './page-common/page-default/page-default.component';
-import {LoginComponent} from './page-common/login/login.component';
-import { ServiceDeleteDialogComponent } from './services-manager/service-delete-dialog/service-delete-dialog.component';
-import {MatDialogModule} from "@angular/material/dialog";
-
 const routes: Routes = [
-  {
-    path: '', component: PageDefaultComponent
-  }
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes), MatDialogModule],
-  exports: [RouterModule],
-  declarations: [ServiceDeleteDialogComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- sprint-3/internet-store-management/src/app/page-common/page-common.module.ts
+++ sprint-3/internet-store-management/src/app/page-common/page-common.module.ts
@@ -1,14 +1,50 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {PageDefaultComponent} from './page-default/page-default.component';
-import {LoginComponent} from './login/login.component';
-import {RouterTestingModule} from '@angular/router/testing';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CommonRoute} from './page-routing.module';
+import {HeaderComponent} from './component/header/header.component';
+import {HomePageComponent} from './component/home-page/home-page.component';
+import {FooterComponent} from './component/footer/footer.component';
+import {MessageComponent} from './component/message/message.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from 'angularx-social-login';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import { ResetPasswordComponent } from './component/reset-password/reset-password.component';
+import { MyGameComponent } from './component/my-game/my-game.component';
 
 @NgModule({
-  declarations: [PageDefaultComponent, LoginComponent],
+  declarations: [MyGameComponent , HeaderComponent, HomePageComponent, FooterComponent, MessageComponent, ResetPasswordComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
-    RouterTestingModule,
-  ]
+    RouterModule.forChild(CommonRoute),
+    FormsModule,
+    SocialLoginModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [{
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider('375466110415778'),
+      },
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider('103585693874-0bjkl21cmmjf8d9n09io95ciuveiievl.apps.googleusercontent.com'),
+        },
+      ],
+    } as SocialAuthServiceConfig,
+  }],
 })
-export class PageCommonModule { }
+export class PageCommonModule {
+}
